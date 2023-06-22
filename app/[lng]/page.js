3b769899cd009ad475c91a15b4cf0c0164d7e09f
Anil Kumar <anilbms75@gmail.com>
2023-06-22 02:28:58 +0200

    experiement

---
+++ app/[lng]/page.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import { useTranslation } from '../i18n'
+
+export default async function Page({ params: { lng } }) {
+    const { t } = await useTranslation(lng)
+  return (
+    <>
+      <h1>{t('title')}</h1>
+      <Link href={`/${lng}/second-page`}>
+        {t('second-page')}
+      </Link>
+    </>
+  )
+}
