5f585ea338e76803a47637fa058a4ffb69097c2d
Anil Kumar <anilbms75@gmail.com>
2023-06-22 09:07:03 +0200

    Update style

--- app/[lng]/my-second-page/page.js
+++ app/[lng]/my-second-page/page.js
@@ -5,8 +5,8 @@ export default async function Page({ params: { lng } }) {
   const { t } = await useTranslation(lng, 'my-second-page')
   return (
     <>
-      <h1>{t('title')}</h1>
-      <Link href={`/${lng}`}>
+      <h1 className='text-4xl text-center text-blue-500 font-bold'>{t('title')}</h1>
+      <Link href={`/${lng}`} className="text-blue-500 hover:text-blue-800 transition duration-200 ease-in-out">
         {t('back-to-home')}
       </Link>
     </>
